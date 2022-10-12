947721705f274d72a51e6309f040ed4f0129bba8
AlessandroFiorentino <alexfiore98@gmail.com>
2022-10-12 11:35:17 +0200

    prova

--- terzo.py
+++ terzo.py
@@ -28,11 +28,9 @@ initial debug will be easier if you know exactly what to expect.)
   numerical inaccuracy of your generator?
 
 """
-
 from scipy.interpolate import InterpolatedUnivariateSpline
 from matplotlib import pyplot as plt
-from scipy.stats import norm
-from random import random
+#from scipy.stats import norm
 import numpy as np
 
 N = 1000 #Global variable for the dimension of an array
@@ -42,41 +40,41 @@ class ProbabilityDensityFunction(InterpolatedUnivariateSpline):
     InterpolatedUnivariateSpline(x, y, w=None, bbox=[None, None], k=3, ext=0, check_finite=False)
     class describes a probability density function.
     """
-    def __init__(self, x, y, a = 3):
+    def __init__(self, x, y, a=3):
         """This is the constructor of the class, x and y are two numpy arrays sampling
         the pdf on a grid of values and a is the degree of the polinomial"""
-        f = InterpolatedUnivariateSpline(x, y, k = a)
-        self._F = f.antiderivative() # Mi tengo da parte la cumulata
+        interpol = InterpolatedUnivariateSpline(x, y, k=a)
+        self._antider = interpol.antiderivative() # Mi tengo da parte la cumulata
         self._xmin = np.min(x)
         self._xmax = np.max(x)
-        self._F_max = self._F(self._xmax)
-        InterpolatedUnivariateSpline.__init__(self, self._F(x)/self._F_max, x, k = a)
+        self._antider_max = self._antider(self._xmax)
+        InterpolatedUnivariateSpline.__init__(self, self._antider(x)/self._antider_max, x, k=a)
 
-    def probIn(self, min, max):
+    def prob_in(self, xmin, xmax):
         """ Method calculating the probability in a given interval
         """
 
-        if min < self._xmin or max > self._xmax:
+        if xmin < self._xmin or xmax > self._xmax:
             raise Exception(f"Bounds are out of range [{self._xmin}, {self._xmax}]")
-        return (self._F(max) - self._F(min))/self._F_max
+        return (self._antider(xmax) - self._antider(xmin))/self._antider_max
 
-    def random(self, size = N):
+    def random(self, size=N):
         """Return an array of random values from the pdf.
         """
-        q = np.random.uniform(low=0., high=1., size=(size,))
-        return self(q)
+        rand_vec = np.random.uniform(low=0., high=1., size=(size,))
+        return self(rand_vec)
 
 
 if __name__ == '__main__':
-    x = np.linspace(0., 1., 30)
-    y = np.linspace(0., 3., 30)
+    VX = np.linspace(0., 1., 30)
+    VY = np.linspace(0., 3., 30)
     #Gaussian
     #x = np.linspace(norm.ppf(0.01), norm.ppf(0.99), 100)
     #y = norm.pdf(x)
-    g = ProbabilityDensityFunction(x, y)
-    print(f"the probability is: {g. probIn(0,0.5)}")
+    G = ProbabilityDensityFunction(VX, VY)
+    print(f"the probability is: {G.prob_in(0,0.5)}")
 
-    plt.plot(range(N), g.random(N), '.')
+    plt.plot(range(N), G.random(N), '.')
 
     #plt.plot(x, y, 'o')
     #plt.plot(x, f(x))
